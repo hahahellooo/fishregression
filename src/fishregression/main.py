08d85fb2e26986c27a3081b0094aa9d47191c241
hahahellooo <hahahello777@gmail.com>
2024-09-09 17:45:03 +0900

    main.py

--- src/fishregression/main.py
+++ src/fishregression/main.py
@@ -12,43 +12,17 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("/fish")
-def fish(length: float, weight:float):
-    """
-    물고기의 종류 판별기
-
-    Args:
-        length (float): 물고기 길이(cm)
-        weight (float): 물고기 무게(g)
-
-    Returns:
-        dict: 물고기 종류를 담은 딕셔너리
-    """
+@app.get("/lr_api")
+def lr_api(length: float):
  ### 모델 불러오기
-    with open("/home/diginori/code/fishmlserv/note/model.pkl", "rb") as f:
+    with open("/home/hahahellooo/code/fishregression/note/model.pkl", "rb") as f:
         fish_model = pickle.load(f)
 
-    prediction = fish_model.predict([[length, weight]])
-
-    fish_class = "빙어"
-    if prediction[0] == 1:
-        fish_class = "도미"
+    prediction = fish_model.predict([[length]])
 
     return {
-                "prediction": fish_class,
+                "prediction": prediction,
                 "length": length, 
                 "weight": weight
             }
-
-
-
-
-
-
-
-
-
-
-
-
 
