81feab2b254fd296a3be965e329253a4943e3622
hahahellooo <hahahello777@gmail.com>
2024-09-09 21:16:50 +0900

    -ing

--- src/fishregression/cli.py
+++ src/fishregression/cli.py
@@ -1,3 +1,37 @@
+import requests
+import json
+
+def lr_api(length):
+    headers = {
+        'accept': 'application/json',
+    }
+
+    params = {
+        'length': length,
+    }
+
+    response = requests.get('http://127.0.0.1:8001/fish_linear_ml_predictor', params=params, headers=headers)
+    data=json.loads(response.text)
+    r=data['prediction']
+    return r
+
+def knn_api(length,weight,n_neighbors=5):
+    headers = {
+        'accept': 'application/json',
+    }
+
+    params = {
+        'n_neighbors': '5',
+        'length': length,
+        'weight': weight,
+    }
+
+    response = requests.get('http://127.0.0.1:8002/fish_ml_predictor', params=params, headers=headers)
+    data=json.loads(response.text)
+    r=data['prediction']
+    
+    return r
+
 def predict():
     length = float(input("물고기의 무게를 입력하세요: "))
 
--- src/fishregression/manager.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def get_model_path():
-    file_name = 'model.pkl'
-    my_path=__file__
-    dir_name = os.path.dirname(my_path)
-    model_path = os.path.join(dir_name, file_name)
-    return model_path
